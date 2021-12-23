5e0902084c3c64b8bc47d1b7091e65d92000529e
Fernanda Uchôa <nanda.uq015@gmail.com>
2021-12-23 01:21:22 -0300

    ajustes gerais e adicionada nova pagina para cada quiz

---
+++ quiz-frontend/src/Quizes/index.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { fetchQuiz } from "../api";
+import QuizList from "./QuizList";
+
+
+import { Quiz } from "./types";
+
+function QuizListView(){
+
+    const [quizes, setQuiz] = useState<Quiz[]>([]);
+
+    useEffect(() => {
+    fetchQuiz()
+    .then(response => setQuiz(response.data))
+    .catch( error => console.log )
+    }, []);
+
+    return(
+            <QuizList quizes = {quizes}/>
+
+    )
+}
+
+export default QuizListView;
